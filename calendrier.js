253e31c9836128b360bec763fe325a1ba2f2ff4b
Frederic Merizen <frederic.merizen@gmail.com>
2014-12-12 01:07:19 +0100

    Façon plus simple d'avoir les cases qui prennent toute l'image

--- calendrier.js
+++ calendrier.js
@@ -6,7 +6,7 @@ function construis_les_cases() {
       var pos_y=25*row;
       var date = column + 1 + 5 * row;
       var caze = $('<div id="janvier'+date +'" class="date" style="left:' + pos_x + '%;'+
-          'top:' + pos_y + '%;background-position-x: '+pos_x+'%; background-position-y: '+pos_y+'%;" > <div>' + date + '</div></div>');
+          'top:' + pos_y + '%" > <div>' + date + '</div></div>');
       body.append(caze);
       add_click(caze, date);
     }
