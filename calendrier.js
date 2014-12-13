32fceb3355405aaa11f83d427b553c47a67b8db9
Frederic Merizen <frederic.merizen@gmail.com>
2014-12-13 06:02:02 +0100

    Cases qui s'ouvrent comme des volets

--- calendrier.js
+++ calendrier.js
@@ -6,12 +6,12 @@ function construis_le_calendrier(calendrier) {
 function construis_les_cases(calendrier) {
   for (var row = 0; row < 4; row++) {
     for (var column = 0; column < 5; column++) {
-      var pos_x = 20*column + 1;
-      var pos_y = 25*row + 2;
+      var pos_x = 20*column;
+      var pos_y = 25*row;
       var date = column + 1 + 5 * row;
       var caze = $('<div class="date" style="left:' + pos_x + '%;'+
           'top:' + pos_y + '%" > <div>' + date + '</div></div>');
-      body.append(caze);
+      calendrier.append(caze);
       add_click(caze, date);
     }
   }
@@ -20,7 +20,7 @@ function construis_les_cases(calendrier) {
 function add_click(caze, date) {
   caze.on('click', function() {
     if (date <= new Date().getDate() ) {
-      $(this).hide();
+      $(this).addClass('ouvert');
     }
   });
 }
