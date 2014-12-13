2198ed3507afc60282cf7055df64f583536f9120
Frederic Merizen <frederic.merizen@gmail.com>
2014-12-13 10:48:15 +0100

    Detache la date de la position

--- calendrier.js
+++ calendrier.js
@@ -4,23 +4,34 @@ function construis_le_calendrier(calendrier) {
 }
 
 function construis_les_cases(calendrier) {
-  for (var row = 0; row < 4; row++) {
-    for (var column = 0; column < 5; column++) {
-      var date = column + 1 + 5 * row;
-      calendrier.append(
-          '<div class="case" style="'
-          + 'left:' + (20*column + 1) + '%;'
-          +'top:' + (25*row + 2) + '%;'
-          + '">' + image(date) + '</div>');
-      var caze = $(
-          '<div class="date" style="'
-          + 'left:' + 20*column + '%;'
-          + 'top:' + 25*row + '%;'
-          + '"> <div>' + date + '</div></div>');
-      calendrier.append(caze);
-      add_click(caze, date);
+    var positions = construis_les_positions(4, 5);
+    for (var date = 1; date <= 20; date++) {
+        var position = positions[date - 1];
+        calendrier.append(
+            '<div class="case" style="'
+            + 'left:' + (position.left + 1) + '%;'
+            +'top:' + (position.top + 2) + '%;'
+            + '">' + image(date) + '</div>');
+        var caze = $(
+            '<div class="date" style="'
+            + 'left:' + position.left + '%;'
+            + 'top:' + position.top + '%;'
+            + '"> <div>' + date + '</div></div>');
+        calendrier.append(caze);
+        add_click(caze, date);
     }
-  }
+}
+
+function construis_les_positions(lignes, colonnes) {
+    var largeur = 100 / colonnes,
+        hauteur = 100 / lignes,
+        positions = [];
+    for (var row = 0; row < lignes; row++) {
+        for (var column = 0; column < colonnes; column++) {
+            positions.push({left: largeur * column, top: hauteur * row});
+        }
+    }
+    return positions;
 }
 
 function image(date) {
