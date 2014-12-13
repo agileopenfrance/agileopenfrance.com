6c988c19f35fe58a9969683d9afd0d2b6be0086a
Frederic Merizen <frederic.merizen@gmail.com>
2014-12-13 05:14:01 +0100

    Ajout d'une grille autour des cases pour faire plus vrai

--- calendrier.js
+++ calendrier.js
@@ -1,11 +1,16 @@
+function construis_le_calendrier() {
+  construis_les_cases();
+  construis_la_grille();
+}
+
 function construis_les_cases() {
-  body = $("body");
-  for (row = 0; row < 4; row++) {
-    for (column = 0; column < 5; column++) {
-      var pos_x=20*column;
-      var pos_y=25*row;
+  var body = $("body");
+  for (var row = 0; row < 4; row++) {
+    for (var column = 0; column < 5; column++) {
+      var pos_x = 20*column + 1;
+      var pos_y = 25*row + 2;
       var date = column + 1 + 5 * row;
-      var caze = $('<div id="janvier'+date +'" class="date" style="left:' + pos_x + '%;'+
+      var caze = $('<div class="date" style="left:' + pos_x + '%;'+
           'top:' + pos_y + '%" > <div>' + date + '</div></div>');
       body.append(caze);
       add_click(caze, date);
@@ -19,4 +24,19 @@ function add_click(caze, date) {
       $(this).hide();
     }
   });
+}
+
+function construis_la_grille() {
+  var body = $("body");
+  for (var row = 0; row < 5; row++) {
+    var pos_y=25*row - 2;
+    var ligne = '<div class="row" style="top:' + pos_y + '%;"></div>';
+    body.append(ligne);
+  }
+
+  for (var column = 0; column < 6; column++) {
+    var pos_x = 20*column - 1;
+    var colonne = '<div class="column" style="left:' + pos_x + '%;"></div>';
+    body.append(colonne);
+  }
 }
